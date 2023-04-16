5012c95f1c1eca0ee94239da45488f62998bb5d4
mrmarkiz <maxtomusiak315@gmail.com>
2023-04-16 14:29:20 +0300

    Add Task2 class

--- dz7/Task2.cs
+++ dz7/Task2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using lab7;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,27 @@ namespace dz7
 {
     internal class Task2
     {
+        public static void Run()
+        {
+            MyArray array = new MyArray(10);
+            int choice;
+            do
+            {
+                Console.Write("Enter what to do(1 - show, 2 - show even, 3 - show odd): ");
+                int.TryParse(Console.ReadLine(), out choice);
+                switch (choice)
+                {
+                    case 1:
+                        array.Show();
+                        break;
+                    case 2:
+                        array.ShowEven();
+                        break;
+                    case 3:
+                        array.ShowOdd();
+                        break;
+                }
+            } while (choice != 0);
+        }
     }
 }
