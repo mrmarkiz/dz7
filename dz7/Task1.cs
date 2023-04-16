d12d419e915e2f2cd1b126c5cf4caed308e3ee96
mrmarkiz <maxtomusiak315@gmail.com>
2023-04-16 14:19:58 +0300

    Add Task1 class

--- dz7/Task1.cs
+++ dz7/Task1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using lab7;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,37 @@ namespace dz7
 {
     internal class Task1
     {
-
+        public static void Run()
+        {
+            MyArray array = new MyArray(10);
+            int choice;
+            do
+            {
+                Console.Write("Enter what to do(1 - show, 2 - less, 3 - greater): ");
+                int.TryParse(Console.ReadLine(), out choice);
+                switch (choice)
+                {
+                    case 1:
+                        array.Show();
+                        break;
+                    case 2:
+                        int less;
+                        Console.Write("Enter number to be less than: ");
+                        if (int.TryParse(Console.ReadLine(), out less))
+                        {
+                            Console.WriteLine($"Result: {array.Less(less)}");
+                        }
+                        break;
+                    case 3:
+                        int greater;
+                        Console.Write("Enter number to be greater than: ");
+                        if (int.TryParse(Console.ReadLine(), out greater))
+                        {
+                            Console.WriteLine($"Result: {array.Greater(greater)}");
+                        }
+                        break;
+                }
+            } while (choice != 0);
+        }
     }
 }
